957e038927c8f2879f1e894519c47485f3682967
knakagami-splunk <133175556+knakagami@users.noreply.github.com>
2026-09-07 17:36:12 +0900

    Fix OTLP exporter import

--- helpers/collector_telemetry.py
+++ helpers/collector_telemetry.py
@@ -15,9 +15,10 @@ from typing import Any, Optional
 
 from opentelemetry import context as otel_context
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, OTLPSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.trace import Span, Status, StatusCode
 
 
